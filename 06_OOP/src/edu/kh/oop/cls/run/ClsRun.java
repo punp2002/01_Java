b8167da225f400bfe440af0e060fa96960e2bc46
punp2002 <punp2002@gmail.com>
2025-02-20 12:54:59 +0900

    OOP2

---
+++ 06_OOP/src/edu/kh/oop/cls/run/ClsRun.java
@@ -0,0 +1,14 @@
+package edu.kh.oop.cls.run;
+
+import edu.kh.oop.cls.model.service.ClslService;
+
+public class ClsRun {
+	public static void main(String[] args) {
+		
+		ClslService service = new ClslService();
+		
+		//service.ex2();
+		//service.ex3();
+		service.ex4();
+	}
+}
