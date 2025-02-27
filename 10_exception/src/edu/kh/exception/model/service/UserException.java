1df95cc514f3704165c6086a36226899b7a82557
punp2002 <punp2002@gmail.com>
2025-02-27 17:09:45 +0900

    컬렉션

---
+++ 10_exception/src/edu/kh/exception/model/service/UserException.java
@@ -0,0 +1,14 @@
+package edu.kh.exception.model.service;
+
+// 사용자 정의 예외 클래스
+public class UserException extends Exception{
+	
+	public UserException() {}
+
+	public UserException(String message) {
+		super(message);
+	}
+
+	
+	
+}
